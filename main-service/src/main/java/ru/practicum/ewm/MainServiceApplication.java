c555b21eeb4e5ec9fda7a5f51b8f679c839fdb9f
yaengine <yaengine@yandex.ru>
2025-06-21 16:50:01 +0300

    Diploma project. Stat SVC.

--- main-service/src/main/java/ru/practicum/ewm/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/ewm/MainServiceApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServiceApplication
-{
+public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
     }
--- stats-service/stats-server/src/main/java/ru/practicum/ewm/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/service/StatsService.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 public interface StatsService {
     void saveHit(EndpointHitDto hitDto);
+
     List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
