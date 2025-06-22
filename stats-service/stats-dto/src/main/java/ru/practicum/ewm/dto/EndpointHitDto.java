8a7fd54b52455a1b974d18a78e9136b9703c8a97
yaengine <yaengine@yandex.ru>
2025-06-22 20:27:55 +0300

    Diploma project. Stat SVC. Add StatsClient

--- stats-service/stats-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 
