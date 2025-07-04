cbe8dd41dc68b8312227eacb3fbdf25b0fa18d08
yaengine <yaengine@yandex.ru>
2025-07-05 01:19:41 +0300

    Diploma project. Main SVC. CheckStyle correction

--- main-service/src/main/java/ru/practicum/ewm/controller/PublicCategoryController.java
+++ main-service/src/main/java/ru/practicum/ewm/controller/PublicCategoryController.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.dto.CategoryDto;
 import ru.practicum.ewm.service.CategoryService;
